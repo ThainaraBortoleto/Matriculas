8d7f89047404984d69196fd3073f1756e744a73b
Thainara Bortoleto <thainarabortoleto@hotmail.com>
2023-10-15 19:00:35 -0300

    header

---
+++ LandingPage/src/services/AuthService.ts
@@ -0,0 +1,12 @@
+import router from "../router"
+export default function auth(events: SubmitEvent) {
+    const formData = new FormData(events.target as HTMLFormElement);
+    console.log({...formData});
+    alert('aqui')
+
+    if (true){
+        router.push('/home')
+    } else {
+        router.push('/login')
+    }
+}
--- LandingPage/src/views/HomeView.vue
+++ LandingPage/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/HeaderComponent.vue';
 </script>
 
 <template>
   <main>
+    <Header />
     <TheWelcome />
   </main>
 </template>
